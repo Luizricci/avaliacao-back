c661143b46d2a0529ca74e0fd907f02a508d3a8a
Luiz_ricci <luizaureliano.tds1.senai@gmail.com>
2025-05-06 09:41:42 -0300

    feat: alter query method

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -6,17 +6,29 @@ CREATE TABLE produtos (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     validade VARCHAR(255) NOT NULL,
-    photo TEXT 
+    photo TEXT,
+    fornecedor_id INTEGER REFERENCES fornecedores(id)
+
 );
 
 CREATE TABLE fornecedores (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    produto_id INTEGER REFERENCES produtos(id)
+    name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO produtos (name, validade, photo) VALUES
-    ('pão de forma', '23-04-2030','photo1'),
-    ('pão de forma', '23-04-2028','photo2'),
-    ('pão de forma', '23-04-2027','photo3'),
-    ('pão de batata', '23-05-2028', 'photo2');
+INSERT INTO produtos (name, validade, photo, fornecedor_id) VALUES
+    ('pão de batata', '23-05-2028', 'photo2', 1),
+    ('pão de forma', '23-05-2027', 'photo3', 2),
+    ('banana', '23-05-2026', 'photo4', 3),
+    ('maça', '23-05-2025', 'photo5', 4),
+    ('batata', '23-05-2024', 'photo6', 1),
+    ('paleta', '23-05-2023', 'photo7', 2),
+    ('tilapia', '23-05-2022', 'photo8', 3);
+
+
+
+INSERT INTO fornecedores (name) VALUES
+    ('fornecedor 1'),
+    ('fornecedor 2'),
+    ('fornecedor 3'),
+    ('fornecedor 4');
--- src/models/ProdutosModel.js
+++ src/models/ProdutosModel.js
@@ -2,10 +2,30 @@ const pool = require("../config/database");
 
 const getAllProdutos = async (validade) => {
     if (!validade) {
-        const result = await pool.query('SELECT * FROM produtos');
+        const result = await pool.query(
+            `SELECT
+                p.id,
+                p.name,
+                p.validade,
+                p.photo,
+                f.name AS fornecedor_name
+            FROM produtos p
+            LEFT JOIN fornecedores f ON p.fornecedor_id = f.id
+            `
+        );
         return result.rows;
     } else {
-        const result = await pool.query('SELECT * FROM produtos WHERE validade ILIKE $1', [`%${validade}%`]);
+        const result = await pool.query(
+            `SELECT
+                p.id,
+                p.name,
+                p.validade,
+                p.photo,
+                f.name AS fornecedor_name
+            FROM produtos p
+            LEFT JOIN fornecedores f ON p.fornecedor_id = f.id
+            WHERE p.validade LIKE $1
+            `, [`%${validade}%`]);
         return result.rows;
     }
 }
